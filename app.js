6091de02f9e89e9fadd920d9709fb028aea59bf7
deliacodes <deliacodes@gmail.com>
2017-10-06 13:49:41 -0700

    roughing out the AJAX call

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 "use strict";
 
-var numverify_URL = 'http://apilayer.net/api/validate?access_key=a7a160a67a230728efce44031b8c27fd';
+//most of below taken from the numverify docs -https://numverify.com/documentation
+// I'm not certain I need to use jsonp. 
+//do I need to add GET to the request?
 
-http://apilayer.net/api/validate
+//should access key be a string?
+var accessKey = 'a7a160a67a230728efce44031b8c27fd';
+var phoneNumber = '' ;
 
-    ? access_key = a7a160a67a230728efce44031b8c27fd
-    & number = 14158586273
-    & country_code = US
-    & format = 1
+// verify phone number via AJAX call
+$.ajax({
+    url: 'http://apilayer.net/api/validate?access_key=' + accessKey + '&number=' + phoneNumber,   
+    dataType: 'jsonp',
+    success: function(json) {
+
+    // Access and use your preferred validation result objects
+    console.log(json.valid);
+    console.log(json.country_code);
+    console.log(json.carrier);
+                
+    }
+});
+
+
