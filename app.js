7f7c8ba19c9b66b9356feaed3dad6344debc3b5b
deliacodes <deliacodes@gmail.com>
2017-10-05 22:12:36 -0700

    building numverify api request

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+var numverify_URL = 'http://apilayer.net/api/validate?access_key=YOUR_ACCESS_KEY  ';
+
+http://apilayer.net/api/validate
+
+    ? access_key = a7a160a67a230728efce44031b8c27fd
+    & number = 14158586273
+    & country_code = US
+    & format = 1
