453c8e4d7b392260093e0d92e948c905affeedce
deliacodes <deliacodes@gmail.com>
2017-10-08 19:28:01 -0700

    small tweaks - renaming json to data. time to merge back to master.

--- app.js
+++ app.js
@@ -2,27 +2,26 @@
 
 //most of below taken from the numverify docs -https://numverify.com/documentation
 // I'm not certain I need to use jsonp. 
-//do I need to add GET to the request? (you don't - default ajax might be get)
 //async loads when it loads - so any data
 
 //should access key be a string?
 var accessKey = 'a7a160a67a230728efce44031b8c27fd';
-var phoneNumber = '' ;
+var phoneNumber = '';
 
 
 // verify phone number via AJAX call
 $('#submit').click(function(){
 	phoneNumber = $('input').val();
-	
+	console.log(phoneNumber);
 $.ajax({
     url: 'http://apilayer.net/api/validate?access_key=' + accessKey + '&number=' + phoneNumber,   
     dataType: 'jsonp',
     success: function(data) {
     //data manipulation here
     // Access and use your preferred validation result objects
-    console.log(json.valid);
-    console.log(json.country_code);
-    console.log(json.carrier);
+    console.log(data.valid);
+    console.log(data.country_code);
+    console.log(data.carrier);
                 
     }
 })
